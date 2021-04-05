42fcf847f20137df6e2e3186eff1dd821f3dfa45
jedmundo-rm <jedmundo.rm@gmail.com>
2021-04-05 18:17:53 -0500

    update

---
+++ Class Exercise/s19-JS/main.js
@@ -0,0 +1,78 @@
+
+function getInitials(name){
+    // separamos en arrays las palabras
+    const nameSplitted = name.split(" ")
+    console.log(nameSplitted)
+
+    // creamos una variable donde almacenar las inciales
+    let initials = ""
+
+    //recorrer cada elemento
+    nameSplitted.forEach((word) => {
+        console.log(word.charAt(0))
+        initials += word.charAt(0) + "."
+        console.log(initials)
+    })
+
+    return initials
+}
+
+getInitials("Jaime Edmundo");
+
+/***************** */
+
+function getVocals(...string){
+    console.log(string)
+
+    const object = {}
+    
+    string.forEach((str) => {
+        // regresa los elementos que coinciden con la coincidencia
+        const quantity = str.match(/[aeiou]/gi).length
+        object[str] = quantity
+    })
+
+    return object
+}
+
+console.log(getVocals("Hola", "Koders", "Hola Koders", "Hola", "Koders"))
+
+/***************** */
+
+arrayKoders = [
+	[
+		["name", "Ruben"],
+		["lastName", "Alvarez"],
+		["generation", 1],
+        	["bootcamp", "python"],
+        	["score", 90]
+	],
+	[
+		["name", "Andre"],
+		["lastName", "Alvarez"],
+		["generation", 1],
+        ["bootcamp", "python"],
+        ["score", 90]
+	]
+]
+
+
+
+const newArrayKoders = arrayKoders.map((arrayproperties)=>{
+    console.log(arrayproperties)
+    const objectKoder = arrayproperties.reduce((object, koderProperty) => {
+        console.log(koderProperty)
+
+        const key = koderProperty[0]
+        const value = koderProperty [1]
+        object[key] = value
+
+        return object
+    }, {})
+
+    console.log(objectKoder)
+    //return objectKoder
+
+    return new koder(objectKoder.name, objectKoder.lastName)
+})
+
