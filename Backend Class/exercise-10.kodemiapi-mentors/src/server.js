c94a0a984d7749a4b4f756b614e2fb83e564c9c4
jedmundo-rm <jedmundo.rm@gmail.com>
2021-05-24 19:41:45 -0500

    middleware

--- Backend Class/exercise-10.kodemiapi-mentors/src/server.js
+++ Backend Class/exercise-10.kodemiapi-mentors/src/server.js
@@ -8,12 +8,26 @@ const kodersRouter = require('./routers/koders')
 //montamos el router
 const mentorsRouter = require('./routers/mentors')
 
+// middleware
+const middlewareLog = require('./middlewares/middleware')
+
+
+
 const app = express()
+
+// esto lo usaos para los middleware
+// Sirve para leer el Body y poder hacer el POST
+app.use(express.json())
+
+// mandamos a llamar el middleware (esto debe estar antes de las llamadas del router)
+app.use(middlewareLog)
 
 // mandamos a llamar el router
 app.use('/koders', kodersRouter)
 
 // mandamos a llamar el router
 app.use('/mentors', mentorsRouter)
+
+
 
 module.exports = app
